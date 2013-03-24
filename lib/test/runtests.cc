e9608cc59c9a097f150c81a86821a1353226aa4c
seu <seu@panopticon.re>
2013-03-24 02:38:47 +0100

    Fix token pattern bug

--- lib/test/runtests.cc
+++ lib/test/runtests.cc
@@ -1,8 +1,11 @@
-#include "test_codegenerator.hh"
+#include "test_disassembler.hh"
+#include "test_procedure.hh"
+
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/ui/text/TestRunner.h>
 
 CPPUNIT_TEST_SUITE_REGISTRATION(CodeGeneratorTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(ProcedureTest);
 
 int main(int argc,char **argv)
 {
