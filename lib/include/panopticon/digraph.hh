3332d467e458f9f3d45de7d28b287cfb4e029e15
seu <seu@panopticon.re>
2014-03-19 22:30:50 +0100

    Fix digraph tests

--- lib/include/panopticon/digraph.hh
+++ lib/include/panopticon/digraph.hh
@@ -16,181 +16,13 @@
 namespace po
 {
 	template<typename N, typename E>
-	using digraph = boost::adjacency_list<boost::vecS,boost::vecS,boost::directedS,boost::property<boost::vertex_name_t,boost::optional<N>>,boost::property<boost::edge_name_t,boost::optional<E>>>;
-}
-/*
-namespace boost
-{
-	template<typename N, typename E>
-	struct graph_traits<po::digraph<N,E>>
-	{
-		using base = adjacency_list<vecS,vecS,directedS,property<vertex_name_t,N>,property<edge_name_t,E>>;
-
-		// Graph concept
-		using vertex_descriptor = typename graph_traits<base>::vertex_descriptor;
-		using edge_descriptor = typename graph_traits<base>::edge_descriptor;
-		using directed_category = typename graph_traits<base>::directed_category;
-		using edge_parallel_category = typename graph_traits<base>::edge_parallel_category;
-		using traversal_category = typename graph_traits<base>::traversal_category;
-
-		// VertexListGraph concept
-		using vertex_iterator = typename graph_traits<base>::vertex_iterator;
-		using vertices_size_type = typename graph_traits<base>::vertices_size_type;
-
-		// EdgeListGraph concept
-		using edge_iterator = typename graph_traits<base>::edge_iterator;
-		using edges_size_type = typename graph_traits<base>::edges_size_type;
-
-		// IncidenceGraph concept
-		using out_edge_iterator = typename graph_traits<base>::out_edge_iterator;
-		using degree_size_type = typename graph_traits<base>::degree_size_type;
-
-		// BidirectionalGraph concept
-		using in_edge_iterator = typename graph_traits<base>::in_edge_iterator;
-	};
-}*/
-
-namespace po
-{
-	/*template<typename N, typename E>
-	struct digraph
-	{
-		using vertex_descriptor = typename boost::graph_traits<digraph<N,E>>::vertex_descriptor;
-		using edge_descriptor = typename boost::graph_traits<digraph<N,E>>::edge_descriptor;
-		using vertex_iterator = typename boost::graph_traits<digraph<N,E>>::vertex_iterator;
-		using edge_iterator = typename boost::graph_traits<digraph<N,E>>::edge_iterator;
-		using out_edge_iterator = typename boost::graph_traits<digraph<N,E>>::out_edge_iterator;
-		using in_edge_iterator = typename boost::graph_traits<digraph<N,E>>::in_edge_iterator;
-		using size_type = size_t;
-
-		digraph(void) : _adjacency_list() {}
-
-		const N &get_node(const vertex_descriptor& n) const
-		{
-			return get(boost::vertex_name_t(),n,_adjacency_list);
-		}
-
-		N &get_node(const vertex_descriptor &n)
-		{
-			return get(boost::vertex_name_t(),n,_adjacency_list);
-		}
-
-		const E &get_edge(const edge_descriptor& e) const
-		{
-			return (_edges.count(e) ? _edges.at(e) : throw std::out_of_range("no such edge"));
-		}
-
-		E &get_edge(const edge_descriptor& e)
-		{
-			return (_edges.count(e) ? _edges[e] : throw std::out_of_range("no such edge"));
-		}
-
-		vertex_descriptor insert_node(const N& n)
-		{
-			vertex_descriptor vx = add_vertex(_adjacency_list);
-			_nodes.emplace(vx,n);
-
-			return vx;
-		}
-
-		edge_descriptor insert_edge(const E& e, vertex_descriptor from, vertex_descriptor to)
-		{
-			auto o = add_edge(from,to,_adjacency_list);
-
-			if(!o.second)
-				throw std::runtime_error("edge exists");
-
-			_edges.emplace(o.first,e);
-
-			return o.first;
-		}
-
-	private:
-		boost::adjacency_list<boost::vecS,boost::vecS,boost::directedS,boost::property<boost::vertex_name_t,N>,boost::property<boost::edge_name_t,E>> _adjacency_list;
-
-		friend std::pair<vertex_iterator,vertex_iterator> vertices(const digraph &);
-	};
-
-	template<typename N, typename E>
-	std::pair<typename boost::graph_traits<digraph<N,E>>::vertex_iterator,
-						typename boost::graph_traits<digraph<N,E>>::vertex_iterator>
-	vertices(const digraph<N,E> &g)
-	{
-		return boost::vertices(g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::vertices_size_type
-	num_vertices(const digraph<N,E> &g)
-	{
-		return boost::num_vertices(g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	std::pair<typename boost::graph_traits<digraph<N,E>>::edge_iterator,
-						typename boost::graph_traits<digraph<N,E>>::edge_iterator>
-	edges(const digraph<N,E> &g)
-	{
-		return boost::edges(g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::edges_size_type
-	num_edges(const digraph<N,E> &g)
-	{
-		return boost::num_edges(g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::vertex_descriptor
-	source(const typename boost::graph_traits<digraph<N,E>>::edge_descriptor &e, const digraph<N,E> &g)
-	{
-		return boost::source(e,g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::vertex_descriptor
-	target(const typename boost::graph_traits<digraph<N,E>>::edge_descriptor &e, const digraph<N,E> &g)
-	{
-		return boost::target(e,g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	std::pair<typename boost::graph_traits<digraph<N,E>>::out_edge_iterator,
-						typename boost::graph_traits<digraph<N,E>>::out_edge_iterator>
-	out_edges(const typename boost::graph_traits<digraph<N,E>>::vertex_descriptor &v, const digraph<N,E> &g)
-	{
-		return boost::out_edges(v,g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::degree_size_type
-	out_degree(const typename boost::graph_traits<digraph<N,E>>::vertex_descriptor &v, const digraph<N,E> &g)
-	{
-		return boost::out_degree(v,g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	std::pair<typename boost::graph_traits<digraph<N,E>>::in_edge_iterator,
-						typename boost::graph_traits<digraph<N,E>>::in_edge_iterator>
-	in_edges(const typename boost::graph_traits<digraph<N,E>>::vertex_descriptor &v, const digraph<N,E> &g)
-	{
-		return boost::in_edges(v,g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::degree_size_type
-	in_degree(const typename boost::graph_traits<digraph<N,E>>::vertex_descriptor &v, const digraph<N,E> &g)
-	{
-		return boost::in_degree(v,g._adjacency_list);
-	}
-
-	template<typename N, typename E>
-	typename boost::graph_traits<digraph<N,E>>::degree_size_type
-	degree(const typename boost::graph_traits<digraph<N,E>>::vertex_descriptor &v, const digraph<N,E> &g)
-	{
-		return boost::degree(v,g._adjacency_list);
-	}*/
+	using digraph = boost::adjacency_list<
+		boost::vecS,
+		boost::vecS,
+		boost::directedS,
+		boost::property<boost::vertex_name_t,boost::optional<N>>,
+		boost::property<boost::edge_name_t,boost::optional<E>>
+	>;
 
 	template<typename I>
 	struct iter_pair
@@ -252,7 +84,11 @@ namespace po
 	find_node(const N& n, const po::digraph<N,E> &g)
 	{
 		auto p = boost::vertices(g);
-		return *std::find_if(p.first,p.second,[&](typename boost::graph_traits<po::digraph<N,E>>::vertex_descriptor v) { return get_node(v,g) == n; });
+		auto i = std::find_if(p.first,p.second,[&](typename boost::graph_traits<po::digraph<N,E>>::vertex_descriptor v) { return get_node(v,g) == n; });
+		if(i != p.second)
+			return *i;
+		else
+			throw std::out_of_range("unknown node");
 	}
 
 	template<typename N,typename E>
@@ -260,7 +96,11 @@ namespace po
 	find_edge(const E& e, const po::digraph<N,E> &g)
 	{
 		auto p = boost::edges(g);
-		return *std::find_if(p.first,p.second,[&](typename boost::graph_traits<po::digraph<N,E>>::edge_descriptor v) { return get_edge(v,g) == e; });
+		auto i = std::find_if(p.first,p.second,[&](typename boost::graph_traits<po::digraph<N,E>>::edge_descriptor v) { return get_edge(v,g) == e; });
+		if(i != p.second)
+			return *i;
+		else
+			throw std::out_of_range("unknown edge");
 	}
 
 	template<typename N, typename E>
--- lib/test/digraph.cc
+++ lib/test/digraph.cc
@@ -2,13 +2,14 @@
 #include <panopticon/digraph.hh>
 
 using namespace po;
+using namespace std;
 
 TEST(digraph,concepts)
 {
 	BOOST_CONCEPT_ASSERT((boost::GraphConcept<po::digraph<int,std::string>>));
 	BOOST_CONCEPT_ASSERT((boost::VertexAndEdgeListGraphConcept<po::digraph<int,std::string>>));
 }
-/*
+
 TEST(digraph,usage)
 {
 	po::digraph<int,std::string> g;
@@ -17,9 +18,9 @@ TEST(digraph,usage)
 	auto n2 = insert_node(13,g);
 	auto n3 = insert_node(1337,g);
 
-	auto e12 = insert_edge("a",n1,n2,g);
-	auto e23 = insert_edge("b",n2,n3,g);
-	auto e31 = insert_edge("c",n3,n1,g);
+	auto e12 = insert_edge(string("a"),n1,n2,g);
+	auto e23 = insert_edge(string("b"),n2,n3,g);
+	auto e31 = insert_edge(string("c"),n3,n1,g);
 
 	ASSERT_NE(n1, n2);
 	ASSERT_NE(n1, n3);
@@ -33,9 +34,9 @@ TEST(digraph,usage)
 	ASSERT_EQ(get_node(n2,g), 13);
 	ASSERT_EQ(get_node(n3,g), 1337);
 
-	ASSERT_EQ(get_edge(e12,g), "a");
-	ASSERT_EQ(get_edge(e23,g), "b");
-	ASSERT_EQ(get_edge(e31,g), "c");
+	ASSERT_EQ(get_edge(e12,g), string("a"));
+	ASSERT_EQ(get_edge(e23,g), string("b"));
+	ASSERT_EQ(get_edge(e31,g), string("c"));
 
 	ASSERT_EQ(num_edges(g), 3);
 	ASSERT_EQ(num_vertices(g), 3);
@@ -52,9 +53,9 @@ TEST(digraph,usage)
 	ASSERT_EQ(out_degree(n3,g), 1);
 
 	remove_edge(e12,g);
-	remove_node(n1,g);
-	remove_node(n2,g);
-	remove_node(n3,g);
+	remove_vertex(n1,g);
+	remove_vertex(n2,g);
+	remove_vertex(n3,g);
 
 	ASSERT_EQ(num_vertices(g), 0);
 	ASSERT_EQ(num_edges(g), 0);
@@ -64,28 +65,28 @@ TEST(digraph,out_iterator)
 {
 	po::digraph<int,std::string> g;
 
-	auto n1 = g.insert_node(42);
-	auto n2 = g.insert_node(13);
-	auto n3 = g.insert_node(1337);
-	auto n4 = g.insert_node(99);
+	auto n1 = insert_node(42,g);
+	auto n2 = insert_node(13,g);
+	auto n3 = insert_node(1337,g);
+	auto n4 = insert_node(99,g);
 
-	auto e12 = g.insert_edge("a",n1,n2);
-	auto e23 = g.insert_edge("b",n2,n3);
-	auto e21 = g.insert_edge("c",n2,n1);
-	auto e14 = g.insert_edge("d",n1,n4);
+	auto e12 = insert_edge(string("a"),n1,n2,g);
+	auto e23 = insert_edge(string("b"),n2,n3,g);
+	auto e21 = insert_edge(string("c"),n2,n1,g);
+	auto e14 = insert_edge(string("d"),n1,n4,g);
 
-	auto i = g.out_edges(n1);
+	auto i = out_edges(n1,g);
 	ASSERT_TRUE((*i.first == e12 && *std::next(i.first) == e14) || (*i.first == e14 && *std::next(i.first) == e12));
 	ASSERT_EQ(std::next(i.first,2), i.second);
 
-	i = g.out_edges(n2);
+	i = out_edges(n2,g);
 	ASSERT_TRUE((*i.first == e23 && *std::next(i.first) == e21) || (*i.first == e21 && *std::next(i.first) == e23));
 	ASSERT_EQ(std::next(i.first,2), i.second);
 
-	i = g.out_edges(n3);
+	i = out_edges(n3,g);
 	ASSERT_EQ(i.first, i.second);
 
-	i = g.out_edges(n4);
+	i = out_edges(n4,g);
 	ASSERT_EQ(i.first, i.second);
 }
 
@@ -93,22 +94,22 @@ TEST(digraph,iterators)
 {
 	po::digraph<int,std::string> g;
 
-	auto n1 = g.insert_node(42);
-	auto n2 = g.insert_node(13);
-	auto n3 = g.insert_node(1337);
-	auto n4 = g.insert_node(99);
+	auto n1 = insert_node(42,g);
+	auto n2 = insert_node(13,g);
+	auto n3 = insert_node(1337,g);
+	auto n4 = insert_node(99,g);
 
-	g.insert_edge("a",n1,n2);
-	g.insert_edge("b",n2,n3);
-	g.insert_edge("c",n2,n1);
-	g.insert_edge("d",n1,n4);
+	insert_edge(string("a"),n1,n2,g);
+	insert_edge(string("b"),n2,n3,g);
+	insert_edge(string("c"),n2,n1,g);
+	insert_edge(string("d"),n1,n4,g);
 
-	auto i = g.nodes();
-	std::unordered_set<decltype(g)::vertex_descriptor> ns;
+	auto i = vertices(g);
+	std::set<decltype(g)::vertex_descriptor> ns;
 	std::for_each(i.first,i.second,[&](const decltype(g)::vertex_descriptor &n) { ASSERT_TRUE(ns.insert(n).second); });
 
-	auto j = g.edges();
-	std::unordered_set<decltype(g)::edge_descriptor> es;
+	auto j = edges(g);
+	std::set<decltype(g)::edge_descriptor> es;
 	std::for_each(j.first,j.second,[&](const decltype(g)::edge_descriptor &n) { ASSERT_TRUE(es.insert(n).second); });
 
 	ASSERT_EQ(ns.size(), 4);
@@ -119,29 +120,26 @@ TEST(digraph,error)
 {
 	po::digraph<int,std::string> g1,g2;
 
-	auto n1 = g1.insert_node(42);
-	auto n2 = g1.insert_node(13);
-	g1.insert_node(13);
+	auto n1 = insert_node(42,g1);
+	auto n2 = insert_node(13,g1);
+	insert_node(13,g1);
 
-	g1.insert_edge("a",n1,n2);
-	g1.insert_edge("b",n1,n2);
+	insert_edge(string("a"),n1,n2,g1);
+	insert_edge(string("b"),n1,n2,g1);
 
-	ASSERT_EQ(g1.num_edges(), 2);
-	ASSERT_EQ(g1.num_nodes(), 2);
+	ASSERT_EQ(num_edges(g1), 2);
+	ASSERT_EQ(num_vertices(g1), 3);
 
-	auto n3 = g2.insert_node(42);
-	ASSERT_THROW(g1.get_node(n3),std::out_of_range);
-	ASSERT_THROW(g1.out_edges(n3),std::out_of_range);
-	ASSERT_THROW(g1.in_edges(n3),std::out_of_range);
+	auto n3 = insert_node(42,g2);
+	ASSERT_THROW(get_node(n3,g1),std::out_of_range);
+	ASSERT_THROW(out_edges(n3,g1),std::out_of_range);
 	ASSERT_THROW(out_degree(n3,g1),std::out_of_range);
-	ASSERT_THROW(in_degree(n3,g1),std::out_of_range);
-	ASSERT_THROW(degree(n3,g1),std::out_of_range);
 
-	auto n4 = g2.insert_node(422);
-	auto e = g2.insert_edge("dd",n3,n4);
-	ASSERT_THROW(g1.get_edge(e),std::out_of_range);
-	ASSERT_THROW(g1.source(e),std::out_of_range);
-	ASSERT_THROW(g1.target(e),std::out_of_range);
+	auto n4 = insert_node(422,g2);
+	auto e = insert_edge(string("dd"),n3,n4,g2);
+	ASSERT_THROW(get_edge(e,g1),std::out_of_range);
+	ASSERT_THROW(source(e,g1),std::out_of_range);
+	ASSERT_THROW(target(e,g1),std::out_of_range);
 
 	ASSERT_NE(n1, n3);
-}*/
+}
