f136bde9c604dc3659603314b7e06d38ce9379d5
seu <seu@panopticon.re>
2014-03-26 08:41:03 +0100

    Keep failed assertions from opening popups on win32

--- lib/test/testsuite.cc
+++ lib/test/testsuite.cc
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
 
+#ifndef _WIN32
+#include <windows.h>
+#endif
+
 using namespace testing;
 
 int main(int argc, char **argv)
 {
-  InitGoogleTest(&argc, argv);
+#ifdef _WIN32
+	// prevent failed assertions from opening a popup
+	SetErrorMode (SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+#endif
+
+	InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
