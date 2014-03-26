4ad59fdfa7057aae49e24a8c548df13f28816716
seu <seu@panopticon.re>
2014-03-26 23:29:20 +0100

    Try some other functions for disabling

--- lib/test/testsuite.cc
+++ lib/test/testsuite.cc
@@ -10,7 +10,10 @@ int main(int argc, char **argv)
 {
 #if defined(WIN32) || defined(_WINDOWS)
 	// prevent failed assertions from opening a popup
-	SetErrorMode (SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+	SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
 #endif
 
 	InitGoogleTest(&argc, argv);
