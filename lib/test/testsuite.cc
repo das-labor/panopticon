f8796831414749e5f96f9a86e27260ef21525f62
seu <seu@panopticon.re>
2014-03-26 22:57:36 +0100

    Extend win32 defines

--- lib/test/testsuite.cc
+++ lib/test/testsuite.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#ifndef _WIN32
+#if defined(WIN32) || defined(_WINDOWS)
 #include <windows.h>
 #endif
 
@@ -8,7 +8,7 @@ using namespace testing;
 
 int main(int argc, char **argv)
 {
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WINDOWS)
 	// prevent failed assertions from opening a popup
 	SetErrorMode (SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
 #endif
