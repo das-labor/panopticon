c9bca1c174acda01e5e7acc46a59c6bdc6aee22a
seu <seu@panopticon.re>
2014-04-05 15:11:40 +0200

    Refactor basic block

--- lib/include/panopticon/basic_block.hh
+++ lib/include/panopticon/basic_block.hh
@@ -28,15 +28,9 @@
 
 namespace po
 {
-	class basic_block;
-	template<class T> class bblock_iterator;
-	template<class T> class bblock_citerator;
-	typedef std::shared_ptr<class basic_block> bblock_ptr;
-	typedef std::shared_ptr<const class basic_block> bblock_cptr;
-	typedef std::weak_ptr<class basic_block> bblock_wptr;
-	typedef std::weak_ptr<const class basic_block> bblock_cwptr;
-
-	typedef std::shared_ptr<const class procedure> proc_cptr;
+	struct basic_block;
+	using bblock_loc = loc<basic_block>;
+	using bblock_wloc = wloc<basic_block>;
 }
 
 #include <panopticon/mnemonic.hh>
@@ -108,7 +102,6 @@ namespace po
 	/// @returns The relation @ref r as UTF-8 string.
 	std::string pretty(relation::Relcode r);
 	std::ostream& operator<<(std::ostream &os, const guard &g);
-	//odotstream &operator<<(odotstream &os, const guard &g);
 
 	/**
 	 * @brief A jump between two basic blocks.
@@ -127,58 +120,11 @@ namespace po
 		ctrans(guard g, rvalue v);
 
 		/// Jump to basic block @ref b under condition @ref g
-		ctrans(guard g, bblock_ptr b);
+		ctrans(guard g, bblock_loc b);
 
 		guard condition;
 		rvalue value;
-		bblock_wptr bblock;
-	};
-
-	//oturtlestream& operator<<(oturtlestream &os, const ctrans &ct);
-	std::string unique_name(const ctrans &ct);
-
-	/// @internal
-	template<>
-	class bblock_iterator<std::list<ctrans>> : public boost::iterator_facade<
-				bblock_iterator<std::list<ctrans>>,
-				bblock_ptr,
-				boost::forward_traversal_tag,
-				bblock_ptr>
-	{
-	public:
-		bblock_iterator(void) : adaptee() {};
-		explicit bblock_iterator(std::list<ctrans>::iterator i, std::list<ctrans>::iterator iend)
-		: adaptee([](const ctrans &ct) -> bool { return ct.bblock.lock().get() != nullptr; },i,iend) {};
-		bblock_iterator &increment(void) { ++adaptee; return *this; };
-		bblock_iterator &decrement(void) { return *this; };
-
-		bblock_ptr dereference(void) const { return adaptee->bblock.lock(); }
-		bool equal(const bblock_iterator &a) const { return adaptee == a.adaptee; }
-
-	private:
-		boost::filter_iterator<std::function<bool(ctrans &ct)>,std::list<ctrans>::iterator> adaptee;
-	};
-
-	/// @internal
-	template<>
-	class bblock_citerator<std::list<ctrans>> : public boost::iterator_facade<
-				bblock_citerator<std::list<ctrans>>,
-				bblock_cptr,
-				boost::forward_traversal_tag,
-				bblock_cptr>
-	{
-	public:
-		bblock_citerator(void) : adaptee() {};
-		explicit bblock_citerator(std::list<ctrans>::const_iterator i, std::list<ctrans>::const_iterator iend)
-		: adaptee([](const ctrans &ct) -> bool { return ct.bblock.lock().get() != nullptr; },i,iend) {};
-		bblock_citerator &increment(void) { ++adaptee; return *this; };
-		bblock_citerator &decrement(void) { return *this; };
-
-		bblock_cptr dereference(void) const { return adaptee->bblock.lock(); }
-		bool equal(const bblock_citerator &a) const { return adaptee == a.adaptee; }
-
-	private:
-		boost::filter_iterator< std::function<bool(const ctrans &ct)>,std::list<ctrans>::const_iterator> adaptee;
+		bblock_wloc bblock;
 	};
 
 	/**
@@ -186,131 +132,28 @@ namespace po
 	 *
 	 * A basic block is a span of mnemonics that are executed in sequence.
 	 */
-	class basic_block
+	struct basic_block
 	{
-	public:
-		static bblock_ptr unmarshal(const rdf::node &n, proc_cptr proc, const rdf::storage &store);
-
 		basic_block(void);
 
-		// iterators
-		typedef bblock_iterator< std::list<ctrans>> pred_iterator;
-		typedef bblock_iterator< std::list<ctrans>> succ_iterator;
-		typedef bblock_citerator< std::list<ctrans>> pred_citerator;
-		typedef bblock_citerator< std::list<ctrans>> succ_citerator;
-		typedef std::list<ctrans>::iterator out_iterator;
-		typedef std::list<ctrans>::iterator in_iterator;
-
-		/// @returns a pair of iterators pointing to the beginning and the end of the list of predecessor basic blocks
-		std::pair<pred_citerator,pred_citerator> predecessors(void) const;
-		/// @returns a pair of iterators pointing to the beginning and the end of the list of successor basic blocks
-		std::pair<succ_citerator,succ_citerator> successors(void) const;
-		/// @returns a pair of iterators pointing to the beginning and the end of the list of predecessor basic blocks
-		std::pair<pred_iterator,pred_iterator> predecessors(void);
-		/// @returns a pair of iterators pointing to the beginning and the end of the list of successor basic blocks
-		std::pair<succ_iterator,succ_iterator> successors(void);
-
-		/// @returns mnemonics this basic block includes
-		const std::vector<mnemonic> &mnemonics(void) const;
-
-		/// @returns all control transfer instances with this basic block as source
-		const std::list<ctrans> &incoming(void) const;
-		/// @returns all control transfer instances with this basic block as destination
-		const std::list<ctrans> &outgoing(void) const;
-
-		// mutates internal lists (mnemonics and incoming/outgoing) and updates m_area, checks invariants
-		//
-		/**
-		 * Calls a given function, supplying a mutable reference to the list of mnemonics.
-		 * The function sanity checks the list after the call and update @ref area.
-		 */
-		void mutate_mnemonics(std::function<void(std::vector<mnemonic>&)> fn);
-
-		/**
-		 * Calls a given function, supplying a mutable reference to the list of incoming control transfers.
-		 * The function sanity checks the list after the call.
-		 */
-		void mutate_incoming(std::function<void(std::list<ctrans>&)> fn);
-
-		/**
-		 * Calls a given function, supplying a mutable reference to the list of outgoing control transfers.
-		 * The function sanity checks the list after the call.
-		 */
-		void mutate_outgoing(std::function<void(std::list<ctrans>&)> fn);
-
-		/// @returns the address range this basic block spans
-		const bound &area(void) const;
-
-		/// Deletes all control transfers and mnemonics inside
-		void clear(void);
+		bound area(void) const;
+		const std::vector<mnemonic>& mnemonics(void) const { return _mnemonics; }
+		std::vector<mnemonic>& mnemonics(void) { _area = boost::none; return _mnemonics; }
 
 	private:
-		void mutate_controlflow(std::list<ctrans> &lst, std::function<void(std::list<ctrans>&)> fn);
-
-		bound m_area;
-		std::vector<mnemonic> m_mnemonics;
-
-		std::list<ctrans> m_incoming;
-		std::list<ctrans> m_outgoing;
+		mutable boost::optional<bound> _area;
+		std::vector<mnemonic> _mnemonics;
 	};
 
-	bool operator<(const bblock_wptr &a, const bblock_wptr &b);
-	bool operator<(const bblock_cwptr &a, const bblock_cwptr &b);
+	template<>
+	rdf::statements marshal(const basic_block*, const uuid&);
 
-	//odotstream &operator<<(odotstream &os, const basic_block &bb);
-	//oturtlestream &operator<<(oturtlestream &os, const basic_block &bb);
-	std::string unique_name(const basic_block &bb);
-
-	/// Iterates all mnemoics in @ref bb, calling @ref f for each instruction.
-	void execute(bblock_cptr bb,std::function<void(const lvalue&,instr::Function,const std::vector<rvalue>&)> f);
+	template<>
+	basic_block* unmarshal(const uuid&, const rdf::storage&);
 
 	/// Iterates all mnemoics in @ref bb, calling @ref f for each instruction.
-	void execute2(bblock_cptr bb,std::function<void(const instr&)> f);
+	void execute(bblock_loc bb,std::function<void(const lvalue&,instr::Function,const std::vector<rvalue>&)> f);
 
-	/**
-	 * Iterates all mnemoics in @ref bb, calling @ref f for each instruction.
-	 * @note Allows modification of the instructions
-	 */
-	void rewrite(bblock_ptr bb,std::function<void(lvalue&,instr::Function,std::vector<rvalue>&)> f);
-
-	/// Adds an control transfer with @ref from as source and @ref to as destination
-	void conditional_jump(bblock_ptr from, bblock_ptr to, guard g);
-	/// Adds an control transfer with @ref from as source and @ref to as destination
-	void conditional_jump(rvalue from, bblock_ptr to, guard g);
-	/// Adds an control transfer with @ref from as source and @ref to as destination
-	void conditional_jump(bblock_ptr from, rvalue to, guard g);
-
-	/// Adds an control transfer with @ref from as source and @ref to as destination
-	void unconditional_jump(bblock_ptr from, bblock_ptr to);
-	/// Adds an control transfer with @ref from as source and @ref to as destination
-	void unconditional_jump(rvalue from, bblock_ptr to);
-	/// Adds an control transfer with @ref from as source and @ref to as destination
-	void unconditional_jump(bblock_ptr from, rvalue to);
-
-	/// Replaces the source basic block @ref oldbb with @ref newbb in all outgoing control transfers of @ref to.
-	void replace_incoming(bblock_ptr to, bblock_ptr oldbb, bblock_ptr newbb);
-	/// Replaces the destination basic block @ref oldbb with @ref newbb in all outgoing control transfers of @ref from.
-	void replace_outgoing(bblock_ptr from, bblock_ptr oldbb, bblock_ptr newbb);
-	/// Sets the source basic block to @ref bb in every incoming control transfer of @ref to that has a source value equal to @ref v
-	void resolve_incoming(bblock_ptr to, rvalue v, bblock_ptr bb);
-	/// Sets the destination basic block to @ref bb in every outgoing control transfer of @ref from that has a destination value equal to @ref v
-	void resolve_outgoing(bblock_ptr from, rvalue v, bblock_ptr bb);
-
-	/**
-	 * Splits the @ref bb into two. If @ref last is true all mnemonics in @ref bb
-	 * up to @ref pos are includes into the first. Otherwise the mnemonic at @ref pos
-	 * is the first in the second basic block.
-	 * @returns Pair of basic blocks.
-	 */
-	std::pair<bblock_ptr,bblock_ptr> split(bblock_ptr bb, offset pos, bool last);
-
-	/// Merges two adjacent basic blocks into one.
-	bblock_ptr merge(bblock_ptr up, bblock_ptr down);
-
-	/// @internal
-	void replace(std::list<ctrans> &lst, bblock_ptr from, bblock_ptr to);
-	/// @internal
-	void resolve(std::list<ctrans> &lst, rvalue v, bblock_ptr bb);
-	/// @internal
-	void conditional_jump(const ctrans &from, const ctrans &to);
+	/// Iterates all mnemoics in @ref bb, calling @ref f for each instruction.
+	void execute(bblock_loc bb,std::function<void(const instr&)> f);
 }
--- lib/src/basic_block.cc
+++ lib/src/basic_block.cc
@@ -4,21 +4,13 @@
 #include <iostream>
 #include <cassert>
 
-#include <basic_block.hh>
-#ifdef NDEBUG
-#error sss
-#endif
+#include <panopticon/basic_block.hh>
+
 using namespace po;
 using namespace std;
 
-/*
- * relation
- */
 relation::relation(rvalue a, Relcode c, rvalue b) : relcode(c), operand1(a), operand2(b) {}
 
-/*
- * guard
- */
 guard::guard(void) : relations() {}
 guard::guard(const guard &g) : relations(g.relations) {}
 guard::guard(guard &&g) : relations(move(g.relations)) {}
@@ -119,215 +111,65 @@ ostream& po::operator<<(ostream &os, const guard &g)
 	return os;
 }
 
-odotstream &po::operator<<(odotstream &os, const guard &g)
-{
-	static_cast<ostringstream &>(os) << g;
-	return os;
-}
-
-/*
- * ctrans
- */
 ctrans::ctrans(struct guard g, rvalue v) : condition(g), value(v), bblock() {}
-ctrans::ctrans(struct guard g, bblock_ptr b) : condition(g), value(), bblock(b) {}
+ctrans::ctrans(struct guard g, bblock_loc b) : condition(g), value(), bblock(b) {}
 
-/*
- * basic_block
- */
-bblock_ptr basic_block::unmarshal(const rdf::node &node, proc_cptr proc, const rdf::storage &store)
+template<>
+rdf::statements po::marshal(const basic_block* bb, const uuid& u)
 {
-	rdf::statement type = store.first(node,"type"_rdf,"BasicBlock"_po);
-	rdf::stream mnes = store.select(node,"include"_po,nullptr);
-	rdf::stream cts = store.select(node,"preceds"_po,nullptr);
-	bblock_ptr ret(new basic_block());
-	std::list<mnemonic> mne_lst;
+	rdf::statements ret;
+	rdf::node root = rdf::ns_local(to_string(u));
 
-	// mnemoics
-	while(!mnes.eof())
+	ret.emplace_back(root,rdf::ns_rdf("type"),rdf::ns_po("BasicBlock"));
+
+	for(const mnemonic& m: bb->mnemonics())
 	{
-		rdf::statement st;
+		uuid uu;
+		rdf::statements mn = marshal<mnemonic>(&m,uu);
 
-		mnes >> st;
-		mne_lst.emplace_back(mnemonic::unmarshal(st.object(),store));
-	}
-
-	mne_lst.sort([](const mnemonic &a, const mnemonic &b)
-		{ return a.area.begin < b.area.begin; });
-
-	ret->mutate_mnemonics([&](vector<mnemonic> &ms)
-		{ move(mne_lst.begin(),mne_lst.end(),inserter(ms,ms.end())); });
-
-	// constrol transfers
-	while(!cts.eof())
-	{
-		rdf::statement st;
-
-		cts >> st;
+		std::move(mn.begin(),mn.end(),back_inserter(ret));
+		ret.emplace_back(root,rdf::ns_po("include"),rdf::lit(to_string(uu)));
 	}
 
 	return ret;
 }
 
-basic_block::basic_block(void)
-: m_area(), m_mnemonics(), m_incoming(), m_outgoing()
-{}
-
-pair<basic_block::pred_citerator,basic_block::pred_citerator> basic_block::predecessors(void) const
-	{ return make_pair(pred_citerator(m_incoming.cbegin(),m_incoming.cend()),pred_citerator(m_incoming.cend(),m_incoming.cend())); }
-
-pair<basic_block::pred_iterator,basic_block::pred_iterator> basic_block::predecessors(void)
-	{ return make_pair(pred_iterator(m_incoming.begin(),m_incoming.end()),pred_iterator(m_incoming.end(),m_incoming.end())); }
-
-pair<basic_block::succ_citerator,basic_block::succ_citerator> basic_block::successors(void) const
-	{ return make_pair(succ_citerator(m_outgoing.cbegin(),m_outgoing.cend()),succ_citerator(m_outgoing.cend(),m_outgoing.cend())); }
-
-pair<basic_block::succ_iterator,basic_block::succ_iterator> basic_block::successors(void)
-	{ return make_pair(succ_iterator(m_outgoing.begin(),m_outgoing.end()),succ_iterator(m_outgoing.end(),m_outgoing.end())); }
-
-const vector<mnemonic> &basic_block::mnemonics(void) const { return m_mnemonics; }
-
-const list<ctrans> &basic_block::incoming(void) const { return m_incoming; }
-const list<ctrans> &basic_block::outgoing(void) const { return m_outgoing; }
-
-void basic_block::mutate_mnemonics(function<void(vector<mnemonic>&)> fn)
+template<>
+basic_block* po::unmarshal(const uuid& u, const rdf::storage& store)
 {
-	fn(m_mnemonics);
+	rdf::node node(to_string(u));
+	rdf::statements mnes = store.find(node,"include"_po);
+	basic_block *ret = new basic_block();
+	std::list<mnemonic> mne_lst;
 
-	// check invariants:
-	// 	- mnemonics span a consecutive area
+	// mnemoics
+	for(const rdf::statement &st: mnes)
+		mne_lst.emplace_back(*unmarshal<mnemonic>(boost::uuids::string_generator()(st.object.as_literal()),store));
 
-	addr_t first = naddr, last = naddr;
-	assert(all_of(m_mnemonics.begin(),m_mnemonics.end(),[&](mnemonic &m)
-	{
-		bool ret = true;
+	mne_lst.sort([](const mnemonic &a, const mnemonic &b)
+		{ return a.area.lower() < b.area.lower(); });
 
-		if(first == naddr)
-			first = m.area.begin;
-		else
-			ret &= first < m.area.begin;
+	std::move(mne_lst.begin(),mne_lst.end(),back_inserter(ret->mnemonics()));
 
-		if(last != naddr)
-			ret &= last == m.area.begin;
-
-		last = m.area.end;
-		return ret;
-	}));
-
-	// update m_area
-	if(m_mnemonics.empty())
-		m_area = bound();
-	else
-		m_area = bound(first,last);
+	return ret;
 }
 
-void basic_block::mutate_incoming(function<void(list<ctrans>&)> fn)
-{
-	fn(m_incoming);
+basic_block::basic_block(void) : _area(boost::none), _mnemonics() {}
 
-	// check invariants:
-	// 	- condition non-null
-	// 	- no paralell edges
-	set<bblock_ptr> bbs;
-	for(const ctrans &ct: incoming())
+bound basic_block::area(void) const
+{
+	if(!_area)
 	{
-		assert(!ct.bblock.lock() || bbs.insert(ct.bblock.lock()).second);
+		_area = bound();
+
+		for(auto m: _mnemonics)
+			_area = *_area & m.area;
 	}
+
+	return *_area;
 }
 
-void basic_block::mutate_outgoing(function<void(list<ctrans>&)> fn)
-{
-	fn(m_outgoing);
-
-	// check invariants:
-	// 	- condition non-null
-	// 	- no paralell edges
-	set<bblock_ptr> bbs;
-	for(const ctrans &ct: outgoing())
-	{
-		assert(!ct.bblock.lock() || bbs.insert(ct.bblock.lock()).second);
-	}
-}
-
-void basic_block::clear(void)
-{
-	m_incoming.clear();
-	m_outgoing.clear();
-	m_mnemonics.clear();
-}
-
-const bound &basic_block::area(void) const { return m_area; }
-
-/*
- * free functions
- */
-bool po::operator<(const bblock_wptr &a, const bblock_wptr &b)
-{
-	return owner_less<bblock_wptr>()(a, b);
-}
-
-bool po::operator<(const bblock_cwptr &a, const bblock_cwptr &b)
-{
-	return owner_less<bblock_cwptr>()(a, b);
-}
-
-odotstream &po::operator<<(odotstream &os, const basic_block &bb)
-{
-	os << unique_name(bb) << " [shape=record,label=<<table BORDER=\"0\" CELLBORDER=\"0\" CELLSPACING=\"0\" ALIGN=\"LEFT\">";
-
-	for(const mnemonic &m: bb.mnemonics())
-		os << m;
-
-	os << "</table>>];" << endl;
-
-	for(const ctrans &ct: bb.outgoing())
-		if(ct.bblock.lock())
-		{
-			os << unique_name(bb) << " -> " << unique_name(*ct.bblock.lock()) << " [label=\"" << ct.condition << "\"];" << endl;
-		}
-		else
-		{
-			os << unique_name(bb) << " -> " << unique_name(ct.value) << " [label=\"" << ct.condition << "\"];" << endl;
-			os << unique_name(ct.value) << " [label=\"" << ct.value << "\"];" << endl;
-		}
-
-	return os;
-}
-
-oturtlestream &po::operator<<(oturtlestream &os, const basic_block &bb)
-{
-	os << "[" << " rdf:type po:BasicBlock;" << endl;
-	for(const mnemonic &mne: bb.mnemonics())
-		os << " po:include " << mne << ";" << endl;
-
-	/*
-	for(const ctrans &ct: bb.outgoing())
-	{
-		os << " po:preceds " << ct << endl;
-	}
-	/
-		if(ct.bblock.lock())
-			os << g << " po:target :" << unique_name(*ct.bblock.lock()) << "." << endl;
-		else
-			os << g << " po:target " << ct.value << "." << endl;
-
-		for(const relation &rel: ct.condition.relations)
-			os << g << " po:condition [ po:left " << rel.operand1
-													 << "; po:right " << rel.operand2
-													 << "; po:relation " << symbolic(rel.relcode)
-													 << "]." << endl;
-	}*/
-
-	os << "];";
-
-	return os;
-}
-
-string po::unique_name(const basic_block &bb)
-{
-	return "bblock_" + to_string(bb.area().begin) + "_" + to_string(bb.area().end);
-}
-
-void po::execute2(bblock_cptr bb,function<void(const instr&)> f)
+void po::execute(bblock_loc bb,function<void(const instr&)> f)
 {
 	size_t sz_mne = bb->mnemonics().size(), i_mne = 0;
 	const mnemonic *ary_mne = bb->mnemonics().data();
@@ -343,235 +185,10 @@ void po::execute2(bblock_cptr bb,function<void(const instr&)> f)
 	}
 }
 
-void po::execute(bblock_cptr bb,function<void(const lvalue &left, instr::Function fn, const vector<rvalue> &right)> f)
+void po::execute(bblock_loc bb,function<void(const lvalue &left, instr::Function fn, const vector<rvalue> &right)> f)
 {
-	execute2(bb,[&](const instr &i)
+	execute(bb,[&](const instr &i)
 	{
 		f(i.left,i.function,i.right);
 	});
-}
-
-void po::rewrite(bblock_ptr bb,function<void(lvalue &,instr::Function,vector<rvalue>&)> f)
-{
-	bb->mutate_mnemonics([&](vector<mnemonic> &ms)
-	{
-		size_t sz_mne = ms.size(), i_mne = 0;
-		mnemonic *ary_mne = ms.data();
-
-		while(i_mne < sz_mne)
-		{
-			mnemonic &mne = ary_mne[i_mne++];
-			size_t sz_instr = mne.instructions.size(), i_instr = 0;
-			instr *ary_instr = mne.instructions.data();
-
-			while(i_instr < sz_instr)
-			{
-				instr &instr = ary_instr[i_instr++];
-
-				f(instr.left,instr.function,instr.right);
-			}
-		}
-	});
-}
-
-void po::conditional_jump(bblock_ptr from, bblock_ptr to, guard g) { ctrans ct_from(g,from), ct_to(g,to); conditional_jump(ct_from,ct_to); }
-void po::conditional_jump(rvalue from, bblock_ptr to, guard g) { ctrans ct_from(g,from), ct_to(g,to); conditional_jump(ct_from,ct_to); }
-void po::conditional_jump(bblock_ptr from, rvalue to, guard g) { ctrans ct_from(g,from), ct_to(g,to); conditional_jump(ct_from,ct_to); }
-
-void po::unconditional_jump(bblock_ptr from, bblock_ptr to) { conditional_jump(from,to,guard()); }
-void po::unconditional_jump(rvalue from, bblock_ptr to) { conditional_jump(from,to,guard()); }
-void po::unconditional_jump(bblock_ptr from, rvalue to) { conditional_jump(from,to,guard()); }
-
-void po::replace_incoming(bblock_ptr to, bblock_ptr oldbb, bblock_ptr newbb)
-{
-	assert(to && oldbb && newbb);
-	to->mutate_incoming([&](list<ctrans> &in)
-	{
-		replace(in,oldbb,newbb);
-	});
-}
-
-void po::replace_outgoing(bblock_ptr from, bblock_ptr oldbb, bblock_ptr newbb)
-{
-	assert(from && oldbb && newbb);
-	from->mutate_outgoing([&](list<ctrans> &out)
-	{
-		replace(out,oldbb,newbb);
-	});
-}
-
-void po::resolve_incoming(bblock_ptr to, rvalue v, bblock_ptr bb)
-{
-	assert(to && bb);
-	to->mutate_incoming([&](list<ctrans> &in)
-	{
-		resolve(in,v,bb);
-	});
-}
-
-void po::resolve_outgoing(bblock_ptr from, rvalue v, bblock_ptr bb)
-{
-	assert(from && bb);
-	from->mutate_outgoing([&](list<ctrans> &out)
-	{
-		resolve(out,v,bb);
-	});
-}
-
-// last == true -> pos is last in `up', last == false -> pos is first in `down'
-pair<bblock_ptr,bblock_ptr> po::split(bblock_ptr bb, addr_t pos, bool last)
-{
-	assert(bb);
-
-	bblock_ptr up(new basic_block()), down(new basic_block());
-	bool sw = false;
-	basic_block::out_iterator j,jend;
-	basic_block::in_iterator k,kend;
-	function<void(bool,bblock_ptr,ctrans)> append = [](bool in, bblock_ptr bb, ctrans ct)
-	{
-		if(in)
-			bb->mutate_incoming([&](list<ctrans> &l) { l.push_back(ct); });
-		else
-			bb->mutate_outgoing([&](list<ctrans> &l) { l.push_back(ct); });
-	};
-
-	// distribute mnemonics under `up' and `down'
-	for_each(bb->mnemonics().begin(),bb->mnemonics().end(),[&](const mnemonic &m)
-	{
-		assert(!m.area.includes(pos) || m.area.begin == pos);
-
-		if(!last)
-			sw |= m.area.includes(pos);
-
-		if(sw)
-			down->mutate_mnemonics([&](vector<mnemonic> &ms) { ms.push_back(m); });
-		else
-			up->mutate_mnemonics([&](vector<mnemonic> &ms) { ms.push_back(m); });
-
-		if(last)
-			sw |= m.area.includes(pos);
-	});
-	assert(sw);
-
-	// move outgoing ctrans to down
-	for_each(bb->outgoing().begin(),bb->outgoing().end(),[&](const ctrans &ct)
-	{
-		if(ct.bblock.lock() == bb)
-		{
-			append(false,down,ctrans(ct.condition,up));
-			append(true,up,ctrans(ct.condition,up));
-		}
-		else
-		{
-			if(ct.bblock.lock())
-			{
-				append(false,down,ctrans(ct.condition,ct.bblock.lock()));
-				ct.bblock.lock()->mutate_incoming([&](list<ctrans> &in)
-				{
-					in.emplace_back(ctrans(ct.condition,down));
-					in.erase(find_if(in.begin(),in.end(),[&](const ctrans &ct)
-						{ return ct.bblock.lock() == bb; }));
-				});
-			}
-			else
-				append(false,down,ctrans(ct.condition,ct.value));
-		}
-	});
-
-	// move incoming edges to up
-	for_each(bb->incoming().begin(),bb->incoming().end(),[&](const ctrans &ct)
-	{
-		if(ct.bblock.lock() == bb)
-		{
-			append(true,up,ctrans(ct.condition,down));
-			append(false,down,ctrans(ct.condition,up));
-		}
-		else
-		{
-			if(ct.bblock.lock())
-			{
-				append(true,up,ctrans(ct.condition,ct.bblock.lock()));
-				ct.bblock.lock()->mutate_outgoing([&](list<ctrans> &out)
-				{
-					out.emplace_back(ctrans(ct.condition,up));
-					out.erase(find_if(out.begin(),out.end(),[&](const ctrans &ct)
-						{ return ct.bblock.lock() == bb; }));
-				});
-			}
-			else
-				append(true,up,ctrans(ct.condition,ct.value));
-		}
-	});
-
-	bb->clear();
-	unconditional_jump(up,down);
-	return make_pair(up,down);
-}
-
-bblock_ptr po::merge(bblock_ptr up, bblock_ptr down)
-{
-	assert(up && down);
-	if(up->area().begin == down->area().end) tie(up,down) = make_pair(down,up);
-	assert(up->area().end == down->area().begin);
-
-	bblock_ptr ret(new basic_block());
-	auto fn = [&ret](const bblock_ptr &bb, const mnemonic &m) { ret->mutate_mnemonics([&](vector<mnemonic> &ms)
-		{ ms.push_back(m); }); };
-
-	for_each(up->mnemonics().begin(),up->mnemonics().end(),bind(fn,up,placeholders::_1));
-	for_each(down->mnemonics().begin(),down->mnemonics().end(),bind(fn,down,placeholders::_1));
-
-	for_each(up->incoming().begin(),up->incoming().end(),[&](const ctrans &ct)
-	{
-		if(ct.bblock.lock())
-			replace_outgoing(ct.bblock.lock(),up,ret);
-		ret->mutate_incoming([&](list<ctrans> &in) { in.emplace_back(ct); });
-	});
-
-	for_each(down->outgoing().begin(),down->outgoing().end(),[&](const ctrans &ct)
-	{
-		if(ct.bblock.lock())
-			replace_incoming(ct.bblock.lock(),down,ret);
-		ret->mutate_outgoing([&](list<ctrans> &out) { out.emplace_back(ct); });
-	});
-
-	up->clear();
-	down->clear();
-	return ret;
-}
-
-void po::replace(list<ctrans> &lst, bblock_ptr from, bblock_ptr to)
-{
-	assert(from && to);
-
-	auto i = lst.begin();
-	while(i != lst.end())
-	{
-		ctrans ct = *i;
-		if(ct.bblock.lock() == from)
-			i = lst.insert(lst.erase(i),ctrans(ct.condition,to));
-		++i;
-	}
-}
-
-void po::resolve(list<ctrans> &lst, rvalue v, bblock_ptr bb)
-{
-	assert(bb);
-
-	auto i = lst.begin();
-	while(i != lst.end())
-	{
-		ctrans ct = *i;
-		if(ct.value == v)
-			i = lst.insert(lst.erase(i),ctrans(ct.condition,bb));
-		++i;
-	}
-}
-
-void po::conditional_jump(const ctrans &from, const ctrans &to)
-{
-	if(from.bblock.lock())
-		from.bblock.lock()->mutate_outgoing([&](list<ctrans> &out) { out.emplace_back(to); });
-	if(to.bblock.lock())
-		to.bblock.lock()->mutate_incoming([&](list<ctrans> &in) { in.emplace_back(from); });
 }
