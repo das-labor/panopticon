ad060a7037ebb23323ba3f461351c8dcfd6760ac
seu <seu@panopticon.re>
2014-05-04 17:58:30 +0200

    Refactor GraphSceneItem

--- qt/src/main.cc
+++ qt/src/main.cc
@@ -7,6 +7,7 @@
 #include "pen.hh"
 #include "selection.hh"
 #include "panopticon.hh"
+#include "sugiyama.hh"
 
 using namespace std;
 
@@ -19,6 +20,7 @@ int main(int argc, char *argv[])
 	qmlRegisterUncreatableType<Session>("Panopticon",1,0,"Session","Use Panopticon.newSession or Panopticon.openSession.");
 	qmlRegisterSingletonType<Panopticon>("Panopticon",1,0,"Panopticon",Panopticon::provider);
 	qmlRegisterType<LinearView>("Panopticon",1,0,"LinearView");
+	qmlRegisterType<Sugiyama>("Panopticon",1,0,"Sugiyama");
 
 	QApplication app(argc, argv);
 
