cdbd7659f6bff883779c0ece4719c914a4577d45
seu <seu@panopticon.re>
2014-09-07 03:26:10 +0200

    Update YCM config

---
+++ qt/.ycm_extra_conf.py
@@ -0,0 +1,5 @@
+import os;
+
+def FlagsForFile(a):
+	dname = os.path.dirname(os.path.abspath(__file__ ))
+	return {'flags': ['-Wall','-std=c++11','-x','c++','-I/usr/include/libarchive','-I/usr/include/qt5/','-I',dname + "/include"], 'do_cache': True}
