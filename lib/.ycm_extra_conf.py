0a71fa622bf663858997de7035f03858d9a4d190
seu <seu@panopticon.re>
2014-12-07 18:16:27 +0100

    add gtest to ycm config

--- lib/.ycm_extra_conf.py
+++ lib/.ycm_extra_conf.py
@@ -2,4 +2,4 @@ import os;
 
 def FlagsForFile(a):
 	dname = os.path.dirname(os.path.abspath(__file__ ))
-	return {'flags': ['-Wall','-std=c++11','-x','c++','-I/usr/include/libarchive','-I/usr/include/qt5/','-I',dname + "/..lib//include",'-I',dname + "/include"], 'do_cache': True}
+	return {'flags': ['-Wall','-std=c++11','-x','c++','-I/usr/include/libarchive','-I/usr/include/qt5/','-I',dname + "/../lib/include",'-I',dname + "/include",'-I',dname + "/../3rdparty/"], 'do_cache': True}
