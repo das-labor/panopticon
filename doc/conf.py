9140a580064e34e21dd66123e0b976750a43fca8
seu <seu@panopticon.re>
2014-12-08 15:22:15 +0100

    doc: exclude internals/* to silence warnings.

--- doc/conf.py
+++ doc/conf.py
@@ -71,7 +71,10 @@ release = 'dev'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = [
+	'_build',
+	'internals/*'
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
