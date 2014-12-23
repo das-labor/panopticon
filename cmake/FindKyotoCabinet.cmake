b779f0658744347d57c45ed63666102cdaa858c5
seu <seu@panopticon.re>
2014-12-23 10:26:16 +0100

    cmake: ternary BUILD_* switches, fix bug in FindPanopticon.cmake

--- cmake/FindKyotoCabinet.cmake
+++ cmake/FindKyotoCabinet.cmake
@@ -10,14 +10,13 @@ pkg_check_modules(PC_KyotoCabinet QUIET kyotocabinet)
 set(KyotoCabinet_DEFINITIONS ${PC_KyotoCabinet_CFLAGS_OTHER})
 
 find_path(KyotoCabinet_INCLUDE_DIR kcdb.h
-          HINTS ${PC_KyotoCabinet_INCLUDEDIR} ${PC_KyotoCabinet_INCLUDE_DIRS}
-          PATH_SUFFIXES libxml2 )
+          HINTS ${PC_KyotoCabinet_INCLUDEDIR} ${PC_KyotoCabinet_INCLUDE_DIRS})
 
 find_library(KyotoCabinet_LIBRARY NAMES libkyotocabinet kyotocabinet
-             HINTS ${PC_KyotoCabinet_LIBDIR} ${PC_KyotoCabinet_LIBRARY_DIRS} )
+             HINTS ${PC_KyotoCabinet_LIBDIR} ${PC_KyotoCabinet_LIBRARY_DIRS})
 
-set(KyotoCabinet_LIBRARIES ${KyotoCabinet_LIBRARY} )
-set(KyotoCabinet_INCLUDE_DIRS ${KyotoCabinet_INCLUDE_DIR} )
+set(KyotoCabinet_LIBRARIES ${KyotoCabinet_LIBRARY})
+set(KyotoCabinet_INCLUDE_DIRS ${KyotoCabinet_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set KyotoCabinet_FOUND to TRUE
@@ -25,4 +24,4 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args("Kyoto Cabinet" DEFAULT_MSG
                                   KyotoCabinet_LIBRARY KyotoCabinet_INCLUDE_DIR)
 
-mark_as_advanced(KyotoCabinet_INCLUDE_DIR KyotoCabinet_LIBRARY )
+mark_as_advanced(KyotoCabinet_INCLUDE_DIR KyotoCabinet_LIBRARY)
--- cmake/FindPanopticon.cmake
+++ cmake/FindPanopticon.cmake
@@ -9,11 +9,11 @@ find_package(PkgConfig)
 pkg_check_modules(PC_Panopticon QUIET panopticon)
 set(Panopticon_DEFINITIONS ${PC_Panopticon_CFLAGS_OTHER})
 
-find_path(Panopticon_INCLUDE_DIR kcdb.h
+find_path(Panopticon_INCLUDE_DIR panopticon/region.hh
           HINTS ${PC_Panopticon_INCLUDEDIR} ${PC_Panopticon_INCLUDE_DIRS}
-          PATH_SUFFIXES libxml2 )
+          PATH_SUFFIXES)
 
-find_library(Panopticon_LIBRARY NAMES libkyotocabinet kyotocabinet
+find_library(Panopticon_LIBRARY NAMES libpanopticon panopticon
              HINTS ${PC_Panopticon_LIBDIR} ${PC_Panopticon_LIBRARY_DIRS} )
 
 set(Panopticon_LIBRARIES ${Panopticon_LIBRARY} )
@@ -25,4 +25,4 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args("Panopticon" DEFAULT_MSG
                                   Panopticon_LIBRARY Panopticon_INCLUDE_DIR)
 
-mark_as_advanced(Panopticon_INCLUDE_DIR Panopticon_LIBRARY )
+mark_as_advanced(Panopticon_INCLUDE_DIR Panopticon_LIBRARY)
