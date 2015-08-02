5c463c89720e2ca55044c0d3688ae3a6558c5aea
seu <seu@panopticon.re>
2015-08-02 13:37:18 +0200

    fix avr all-opcode test

--- tests/avr.rs
+++ tests/avr.rs
@@ -1,18 +1,18 @@
 extern crate panopticon;
 
 use panopticon::region::Region;
-use panopticon::avr::{AvrState,disassembler};
+use panopticon::avr::{Mcu,disassembler,Avr};
 use panopticon::disassembler::State;
 use std::path::Path;
 
 #[test]
 fn avr_opcodes_01() {
-    let reg = Region::open("flash".to_string(),Path::new("tests/data/avr/all.bin")).unwrap();
+    let reg = Region::open("flash".to_string(),Path::new("tests/data/avr-all-opcodes.bin")).unwrap();
     let main = disassembler();
     let mut addr = 0;
 
     loop {
-        let mut st = State::<u16>::new(addr);
+        let st = State::<Avr>::new(addr,Mcu::new());
         let mut i = reg.iter().seek(addr);
 
         let maybe_match = main.next_match(&mut i,st);
@@ -22,8 +22,9 @@ fn avr_opcodes_01() {
                 println!("{:x}: {}",mne.area.start,mne.opcode);
                 addr = mne.area.end;
             }
-        } else {
+        } else if addr < reg.size() {
             unreachable!("failed to match anything at {:x}",addr);
+        } else {
             break;
         }
     }
