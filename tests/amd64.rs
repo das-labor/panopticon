7ca135c7da875599f21774757de423de7a12ea43
seu <seu@panopticon.re>
2015-08-24 15:17:12 +0200

    x86 tests

---
+++ tests/amd64.rs
@@ -0,0 +1,63 @@
+extern crate panopticon;
+extern crate graph_algos;
+
+use panopticon::region::Region;
+use panopticon::amd64::{disassembler,Amd64,Config,Mode};
+use panopticon::function::{ControlFlowTarget,Function};
+use panopticon::disassembler::State;
+use panopticon::value::Rvalue;
+
+use std::path::Path;
+use std::hash::{Hash,Hasher,SipHasher};
+
+use graph_algos::traits::{VertexListGraph,Graph,MutableGraph,IncidenceGraph,EdgeListGraph};
+
+#[test]
+fn amd64_opcodes() {
+    let reg = Region::open("com".to_string(),Path::new("tests/data/amd64.com")).unwrap();
+    let main = disassembler(64);
+    let mut addr = 0;
+
+    loop {
+        let st = State::<Amd64>::new(addr,Config::new(Mode::Long));
+        let mut i = reg.iter().seek(addr);
+
+        let maybe_match = main.next_match(&mut i,st);
+
+        if let Some(match_st) = maybe_match {
+            for mne in match_st.mnemonics {
+                println!("{:x}: {}",mne.area.start,mne.opcode);
+                addr = mne.area.end;
+            }
+        } else if addr < reg.size() {
+            unreachable!("failed to match anything at {:x}",addr);
+        } else {
+            break;
+        }
+    }
+}
+
+#[test]
+fn ia32_opcodes() {
+    let reg = Region::open("com".to_string(),Path::new("tests/data/ia32.com")).unwrap();
+    let main = disassembler(32);
+    let mut addr = 0;
+
+    loop {
+        let st = State::<Amd64>::new(addr,Config::new(Mode::Long));
+        let mut i = reg.iter().seek(addr);
+
+        let maybe_match = main.next_match(&mut i,st);
+
+        if let Some(match_st) = maybe_match {
+            for mne in match_st.mnemonics {
+                println!("{:x}: {}",mne.area.start,mne.opcode);
+                addr = mne.area.end;
+            }
+        } else if addr < reg.size() {
+            unreachable!("failed to match anything at {:x}",addr);
+        } else {
+            break;
+        }
+    }
+}
