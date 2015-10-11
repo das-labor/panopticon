d778159aac3798365e1ada38e0bfe8325d53b1d6
seu <seu@panopticon.re>
2015-10-11 18:04:14 +0200

    remove project::open test

--- tests/project.rs
+++ tests/project.rs
@@ -4,19 +4,6 @@ use std::path::Path;
 use panopticon::project::Project;
 
 #[test]
-fn open() {
-    /*
-    let maybe_p = Project::new("".to_string());
-
-    assert!(maybe_p.is_some());
-
-    let p = maybe_p.unwrap();
-    assert_eq!(p.name, "test-project".to_string());
-    assert_eq!(p.code, Vec::new());*/
-    assert!(false);
-}
-
-#[test]
 fn pe() {
     let maybe_project = Project::pe(Path::new("tests/data/test.exe"));
 
