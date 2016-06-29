17551b56d6c2b1ca9f2104f17a4bffb30cadadac
seu <seu@panopticon.re>
2016-06-29 21:29:55 +0200

    allow logging in qtpanopticon

--- qt/src/main.rs
+++ qt/src/main.rs
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#[macro_use]
+extern crate log;
+
 extern crate panopticon;
 extern crate qmlrs;
 extern crate libc;
