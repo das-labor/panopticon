45347994f5716a5c88ce48fae759674e02c05aa1
seu <seu@panopticon.re>
2016-10-12 00:12:27 +0200

    travis nightlies 1st try

--- pkg/osx/package_dmg.sh
+++ pkg/osx/package_dmg.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-QTDIR=/Users/user/Qt/5.4/clang_64
-
-QTDIR64=$QTDIR cargo build --release
+cargo build --release
 mkdir -p Panopticon.app/Contents/{MacOS,Resources}
 cp Info.plist Panopticon.app/Contents
 cp ../../target/release/qtpanopticon Panopticon.app/Contents/MacOS/Panopticon
 cp -R ../../qml Panopticon.app/Contents/Resources
-$QTDIR/bin/macdeployqt Panopticon.app -qmldir=Panopticon.app/Contents/Resources/qml/ -dmg
+$QTDIR64/bin/macdeployqt Panopticon.app -qmldir=Panopticon.app/Contents/Resources/qml/ -dmg
