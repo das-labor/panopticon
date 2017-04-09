4b62ca7726c3f4a8d09e3a031865317e68265131
seu <seu@panopticon.re>
2017-04-09 23:55:11 +0200

    run disassembly

---
+++ lib/src/pipeline.rs
@@ -0,0 +1,60 @@
+use futures::sync::mpsc;
+use futures::{stream,Stream,Sink,Future};
+use std::thread;
+use std::collections::{
+    HashSet,
+    HashMap,
+};
+use std::iter::FromIterator;
+use {
+    Architecture,
+    Program,
+    Function,
+    Region,
+    CallTarget,
+    Rvalue,
+};
+use graph_algos::{
+    VertexListGraphTrait,
+    GraphTrait,
+};
+use std::fmt::Debug;
+//use rayon::prelude::*;
+
+pub fn pipeline<A: Architecture + Debug + 'static >(program: Program, region: Region, config: A::Configuration) -> Box<Stream<Item=Function,Error=()> + Send>
+where A::Configuration: Debug {
+    let (tx,rx) = mpsc::channel::<Function>(10);
+    let name = region.name().clone();
+    thread::spawn(move || {
+        let mut tx = tx;
+        let mut functions = HashMap::<u64,Function>::new();
+        let mut targets = HashSet::<u64>::from_iter(program.call_graph.vertices().filter_map(|vx| {
+            match program.call_graph.vertex_label(vx) {
+                Some(&CallTarget::Todo(Rvalue::Constant{ value: ref entry,.. },ref maybe_name,ref uuid)) => Some(*entry),
+                Some(a) => None,
+                None => unreachable!(),
+            }
+        }));
+
+        while !targets.is_empty() {
+            let (new_targets,new_fns): (Vec<Vec<u64>>,Vec<Function>) = targets.into_iter().map(|entry| {
+                let f = Function::disassemble::<A>(None,config.clone(),&region,entry);
+                let new_ct = f.collect_calls().into_iter().filter_map(|rv| {
+                    if let Rvalue::Constant{ value,.. } = rv {
+                        if !functions.contains_key(&value) {
+                            return Some(value);
+                        }
+                    }
+                    None
+                }).collect::<Vec<u64>>();
+
+                functions.insert(entry,f.clone());
+                (new_ct,f)
+            }).unzip();
+            targets = new_targets.into_iter().flat_map(|x| x).collect();
+            tx = tx.send_all(stream::iter(new_fns.into_iter().map(|x| Ok(x)))).wait().unwrap().0;
+        }
+    });
+
+    Box::new(rx)
+}
