4f916cc6748e3bab8a087b811e3aca77d2a9b342
m4b <m4b.github.io@gmail.com>
2017-06-18 21:22:40 -0700

    cli: switch to structopt; add a basic function search option.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
+extern crate structopt;
 #[macro_use]
-extern crate clap;
+extern crate structopt_derive;
 #[macro_use]
 extern crate error_chain;
 extern crate panopticon_core;
@@ -20,6 +21,7 @@ use panopticon_avr as avr;
 use panopticon_analysis::pipeline;
 use panopticon_graph_algos::{GraphTrait};
 use futures::Stream;
+use structopt::StructOpt;
 
 mod errors {
     error_chain! {
@@ -30,39 +32,26 @@ mod errors {
 }
 use errors::*;
 
-fn main() {
-    env_logger::init().unwrap();
-
-    let matches = clap_app!(Panopticon =>
-        (version: "0.1")
-        (about: "A libre cross-platform disassembler.")
-        (@arg INPUT: +required ... {exists_path_val} "Files to disassemble")
-    ).get_matches();
-
-    if let Some(vals) = matches.values_of("INPUT") {
-        for p in vals.map(str::to_string) {
-            info!("{}",p);
-
-            match disassemble(&p) {
-                Ok(()) => {}
-                Err(s) => {
-                    error!("Error: {}",s);
-                    return;
-                }
-            }
-        }
-    }
+#[derive(StructOpt, Debug)]
+#[structopt(name = "panop", about = "A libre cross-platform disassembler.")]
+struct Args {
+    /// The specific function to disassemble
+    #[structopt(short = "f", long = "function", help = "Disassemble the given function")]
+    function: Option<String>,
+    /// The binary to disassemble
+    #[structopt(help = "The binary to disassemble")]
+    binary: String,
 }
 
-fn exists_path_val(filepath: String) -> result::Result<(), String> {
-    match Path::new(&filepath).is_file() {
+fn exists_path_val(filepath: &str) -> result::Result<(), String> {
+    match Path::new(filepath).is_file() {
         true => Ok(()),
         false => Err(format!("'{}': no such file", filepath)),
     }
 }
 
-fn disassemble(p: &String) -> Result<()> {
-    let (mut proj, machine) = loader::load(Path::new(p))?;
+fn disassemble(binary: &str, filter: &Option<String>) -> Result<()> {
+    let (mut proj, machine) = loader::load(Path::new(binary))?;
     let maybe_prog = proj.code.pop();
     let reg = proj.data.dependencies.vertex_label(proj.data.root).unwrap().clone();
 
@@ -77,11 +66,32 @@ fn disassemble(p: &String) -> Result<()> {
         for i in pipe.wait() {
             if let Ok(func) = i {
                 info!("{}",func.uuid);
+                if let &Some(ref filter) = filter {
+                    if func.name.as_str() == filter {
+                        info!("Filter matches {} - {:?}", func.name, func.cflow_graph);
+                    }
+                }
             }
         }
         info!("disassembly thread finished");
-
     }
 
     Ok(())
+}
+
+fn run(args: Args) -> Result<()> {
+    exists_path_val(&args.binary)?;
+    disassemble(&args.binary, &args.function)?;
+    Ok(())
+}
+
+fn main() {
+    env_logger::init().unwrap();
+    match run(Args::from_args()) {
+        Ok(()) => {}
+        Err(s) => {
+            error!("Error: {}",s);
+            ::std::process::exit(1);
+        }
+    }
 }
