1166fc05d297ee8d2bbe787ecc06dbd108e48ff9
seu <seu@panopticon.re>
2017-10-15 22:53:35 +0200

    data flow bench

---
+++ benchmark/benches/data_flow.rs
@@ -0,0 +1,51 @@
+use bencher::Bencher;
+use panopticon_amd64 as amd64;
+use panopticon_core::neo;
+use panopticon_data_flow::neo::rewrite_to_ssa;
+use panopticon_data_flow::ssa_convertion;
+
+fn ssa_convertion_new(b: &mut Bencher) {
+    use panopticon_core::{loader,neo,CallTarget,Rvalue};
+    use panopticon_graph_algos::{VertexListGraphTrait,GraphTrait};
+    use std::path::Path;
+
+    let (proj,_) = loader::load(Path::new("../test-data/static")).unwrap();
+    let entries = proj.code[0].call_graph.vertices().filter_map(|vx| if let Some(&CallTarget::Todo(Rvalue::Constant{ value,.. },_,_)) = proj.code[0].call_graph.vertex_label(vx) { Some(value) } else { None }).collect::<Vec<_>>();
+    let reg = proj.data.dependencies.vertex_label(proj.data.root).unwrap();
+    let mut funcs = vec![];
+
+    for &ep in entries.iter() {
+        let func = neo::Function::new::<amd64::Amd64>(amd64::Mode::Long,ep,&reg,Some("".into())).unwrap();
+        funcs.push(func);
+    }
+
+    b.bench_n(1,|b| {
+        b.iter(|| {
+            for f in funcs.iter_mut() { rewrite_to_ssa(f).unwrap(); }
+        });
+    });
+}
+
+fn ssa_convertion_old(b: &mut Bencher) {
+    use panopticon_core::{loader,Function,CallTarget,Rvalue};
+    use panopticon_graph_algos::{VertexListGraphTrait,GraphTrait};
+    use std::path::Path;
+
+    let (proj,_) = loader::load(Path::new("../test-data/static")).unwrap();
+    let entries = proj.code[0].call_graph.vertices().filter_map(|vx| if let Some(&CallTarget::Todo(Rvalue::Constant{ value,.. },_,_)) = proj.code[0].call_graph.vertex_label(vx) { Some(value) } else { None }).collect::<Vec<_>>();
+    let reg = proj.data.dependencies.vertex_label(proj.data.root).unwrap();
+    let mut funcs = vec![];
+
+    for &ep in entries.iter() {
+        let func = Function::new::<amd64::Amd64>(ep,&reg,None,amd64::Mode::Long).unwrap();
+        funcs.push(func);
+    }
+
+    b.bench_n(1,|b| {
+        b.iter(|| {
+            for f in funcs.iter_mut() { ssa_convertion(f).unwrap(); }
+        });
+    });
+}
+
+benchmark_group!(ssa, ssa_convertion_new, ssa_convertion_old);
---
+++ benchmark/benches/main.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate bencher;
+extern crate panopticon_amd64;
+extern crate panopticon_graph_algos;
+extern crate panopticon_data_flow;
+extern crate panopticon_core;
+
+mod core;
+mod data_flow;
+
+
+benchmark_main!(core::disassemble, data_flow::ssa);
